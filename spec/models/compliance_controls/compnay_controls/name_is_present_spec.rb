cda618d9d09854e258384e3ebbd0941741fe9364
Zog <johan.van-ryseghem@af83.com>
2018-06-05 09:34:57 +0200

    Refs #6938; Company Name compliance check
    And 1st refactor

---
+++ spec/models/compliance_controls/compnay_controls/name_is_present_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe CompanyControl::NameIsPresent, :type => :model do
+  let!(:line_referential){ create :line_referential }
+  let!(:referential){ create :referential, line_referential: line_referential }
+  let!(:company){ create :company, line_referential: line_referential }
+  let(:control_attributes){
+    {}
+  }
+  let(:company_name){ "A NAME" }
+  let(:criticity){ "error" }
+  let(:compliance_check_set){ create :compliance_check_set, referential: referential}
+  let(:compliance_check){
+    create :compliance_check,
+      iev_enabled_check: false,
+      compliance_control_name: "CompanyControl::NameIsPresent",
+      control_attributes: control_attributes,
+      compliance_check_set: compliance_check_set,
+      criticity: criticity
+  }
+
+  before(:each){
+    referential.switch do
+      Chouette::Company.where(id: company.id).update_all(name: company_name)
+    end
+  }
+
+  context "when the company has a name" do
+    it "should pass" do
+      expect{compliance_check.process}.to change{ComplianceCheckResource.count}.by 1
+      resource = ComplianceCheckResource.last
+      expect(resource.status).to eq "OK"
+    end
+  end
+
+  context "when the company has no name" do
+    let(:company_name){ "" }
+
+    context "when the criticity is warning" do
+      let(:criticity){ "warning" }
+
+      it "should set the status according to its params" do
+        expect{compliance_check.process}.to change{ComplianceCheckResource.count}.by 1
+        resource = ComplianceCheckResource.last
+        expect(resource.status).to eq "WARNING"
+      end
+    end
+
+    context "when the criticity is error" do
+      it "should set the status according to its params" do
+        expect{compliance_check.process}.to change{ComplianceCheckResource.count}.by 1
+        resource = ComplianceCheckResource.last
+        expect(resource.status).to eq "ERROR"
+      end
+    end
+  end
+end
