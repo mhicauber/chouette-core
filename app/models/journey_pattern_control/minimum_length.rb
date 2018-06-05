f6b55b68c66fd69b1226ad1cb0d853b7e7c9d033
cedricnjanga <ced.njg@gmail.com>
2018-06-05 10:15:44 -0700

    Refs #6936 JourneyPattern Control on minimum length

---
+++ app/models/journey_pattern_control/minimum_length.rb
@@ -0,0 +1,25 @@
+module JourneyPatternControl
+  class MinimumLength < InternalControl::Base
+    enumerize :criticity, in: %i(error), scope: true, default: :error
+
+    MINIMUM_LENGTH = 2
+
+    def self.default_code; "3-JourneyPattern-3" end
+
+    def self.object_path compliance_check, journey_pattern
+      referential_line_route_journey_patterns_collection_path(journey_pattern.referential, journey_pattern.route.line, journey_pattern.route, journey_pattern)
+    end
+
+    def self.collection referential
+      Chouette::JourneyPattern.where route_id: referential.routes.pluck(:id)
+    end
+
+    def self.compliance_test compliance_check, journey_pattern
+      journey_pattern.stop_points.length >= MINIMUM_LENGTH
+    end
+
+    def self.custom_message_attributes compliance_check, journey_pattern
+      {source_objectid: journey_pattern.objectid}
+    end
+  end
+end
