b15ee54d876b4c4c943cf3120ce814b3188a813b
Zog <johan.van-ryseghem@af83.com>
2018-06-05 16:57:25 +0200

    Refs #6930; Check journeys have purchase windows

--- app/models/vehicle_journey_control/bus_capacity.rb
+++ app/models/vehicle_journey_control/bus_capacity.rb
@@ -1,25 +1,9 @@
 module VehicleJourneyControl
-  class BusCapacity < InternalControl::Base
+  class BusCapacity < InternalBase
     def self.default_code; "3-VehicleJourney-9" end
-
-    def self.object_path compliance_check, vehicle_journey
-      referential_line_route_vehicle_journeys_collection_path(compliance_check.referential, vehicle_journey.route.line, vehicle_journey.route)
-    end
-
-    def self.collection referential
-      referential.vehicle_journeys
-    end
 
     def self.compliance_test compliance_check, vehicle_journey
       vehicle_journey.custom_fields[:capacity]&.value.present?
-    end
-
-    def self.custom_message_attributes compliance_check, vehicle_journey
-      {vehicle_journey_name: vehicle_journey.published_journey_name}
-    end
-
-    def self.label_attr
-      :published_journey_name
     end
   end
 end
