36e4eb2629f63014ea90b979b103e775c6875e99
Zog <johan.van-ryseghem@af83.com>
2018-06-05 12:29:55 +0200

    Refs #6939; Check bus capacity is present on journeys

--- app/models/compliance_check_resource.rb
+++ app/models/compliance_check_resource.rb
@@ -2,6 +2,7 @@ class ComplianceCheckResource < ApplicationModel
   extend Enumerize
 
   belongs_to :compliance_check_set
+  has_many :compliance_check_messages
 
   enumerize :status, in: %i(OK ERROR WARNING IGNORED)
 
