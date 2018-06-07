24c97550e3bf2d5a473ad9b3562813e7368d5b95
Zog <johan.van-ryseghem@af83.com>
2018-06-07 15:46:04 +0200

    Refs #6935; 3-Route-11
    Slightly different implementation than the one asked. To be discussed.

--- app/models/route_control/stop_points_boarding_and_alighting.rb
+++ app/models/route_control/stop_points_boarding_and_alighting.rb
@@ -1,21 +1,9 @@
 module RouteControl
-  class StopPointsBoardingAndAlighting < InternalControl::Base
+  class StopPointsBoardingAndAlighting < InternalBase
     def self.default_code; "3-Route-12" end
-
-    def self.object_path compliance_check, route
-      referential_line_route_path(route.referential, route.line, route)
-    end
-
-    def self.collection referential
-      referential.routes
-    end
 
     def self.compliance_test compliance_check, route
       route.stop_points.commercial.all? {|sp| sp.for_boarding == "forbidden" && sp.for_alighting == "forbidden" }
-    end
-
-    def self.custom_message_attributes compliance_check, route
-      {source_objectid: route.objectid}
     end
   end
 end
