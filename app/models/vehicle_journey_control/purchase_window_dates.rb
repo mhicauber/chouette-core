b065fb9694ef7b93af528e548e92e7e91af89c79
Zog <johan.van-ryseghem@af83.com>
2018-06-12 09:02:33 +0200

    Refs #6979; Fix specs

--- app/models/vehicle_journey_control/purchase_window_dates.rb
+++ app/models/vehicle_journey_control/purchase_window_dates.rb
@@ -1,10 +1,6 @@
 module VehicleJourneyControl
-  class PurchaseWindowDates < InternalControl::Base
+  class PurchaseWindowDates < InternalBase
     def self.default_code; "3-VehicleJourney-7" end
-
-    def self.object_path compliance_check, vehicle_journey
-      referential_line_route_vehicle_journeys_collection_path(compliance_check.referential, vehicle_journey.route.line, vehicle_journey.route)
-    end
 
     def self.collection referential
       referential.vehicle_journeys.joins(:time_tables, :purchase_windows)
@@ -12,10 +8,6 @@ module VehicleJourneyControl
 
     def self.compliance_test compliance_check, vehicle_journey
       vehicle_journey.selling_bounding_dates.last <= vehicle_journey.bounding_dates.last
-    end
-
-    def self.custom_message_attributes compliance_check, vehicle_journey
-      {source_objectid: vehicle_journey.objectid}
     end
   end
 end
