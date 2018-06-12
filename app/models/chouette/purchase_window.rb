14bb8cc0369285fd2da2a10a8e606bd3c2968e97
cedricnjanga <ced.njg@gmail.com>
2018-06-12 07:04:17 -0700

    Refs #6838 Display validation errors on date_ranges

--- app/models/chouette/purchase_window.rb
+++ app/models/chouette/purchase_window.rb
@@ -14,7 +14,8 @@ module Chouette
     belongs_to :referential
     has_and_belongs_to_many :vehicle_journeys, :class_name => 'Chouette::VehicleJourney'
 
-    validates_presence_of :name, :referential, :date_ranges
+    validates_presence_of :name, :referential
+    validates :date_ranges, presence: {message: I18n.t('activerecord.errors.models.purchase_window.attributes.date_ranges.missing')}
 
     scope :contains_date, ->(date) { where('date ? <@ any (date_ranges)', date) }
     scope :overlap_dates, ->(date_range) { where('daterange(?, ?) && any (date_ranges)', date_range.first, date_range.last + 1.day) }
