91743439ad4d8f69fea0ac91c3428739b993996b
Zog <johan@about-blank.fr>
2018-06-28 12:15:16 +0200

    Fix BusCapacity Control

--- app/models/vehicle_journey_control/bus_capacity.rb
+++ app/models/vehicle_journey_control/bus_capacity.rb
@@ -1,11 +1,11 @@
 module VehicleJourneyControl
   class BusCapacity < InternalBase
     required_features :core_controls
-    
+
     def self.default_code; "3-VehicleJourney-9" end
 
     def self.compliance_test compliance_check, vehicle_journey
-      vehicle_journey.custom_fields[:capacity]&.value.present?
+      vehicle_journey.custom_fields[:capacity]&.display_value.present?
     end
   end
 end
