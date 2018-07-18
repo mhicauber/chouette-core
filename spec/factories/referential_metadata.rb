b6d07e31e2a3f223e60a36034c755729b5f4304d
Alban Peignier <alban.peignier@af83.com>
2018-07-18 13:42:17 +0200

    Use the current year as Referential default periodes. Refs #7642

--- spec/factories/referential_metadata.rb
+++ spec/factories/referential_metadata.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :referential_metadata, :class => 'ReferentialMetadata' do
     referential
-    periodes { [ generate(:period) ] }
+    periodes { [ Date.today.beginning_of_year..Date.today.end_of_year ] }
     lines { create_list(:line, 3) }
   end
 
   sequence :period do |n|
     date = Date.today + 2*n
-    date..(date+1)
+    date..(date+10)
   end
 end
