7f6cb6d7a6089075cd59e1e32958d4cc10805c29
Alban Peignier <alban.peignier@af83.com>
2018-07-18 23:21:53 +0200

    Uses match_array to test Subscription organisation features. Refs #7334

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,9 +1,6 @@
-describe Referential, :type => :model do
-  it "should create an Organisation with all features" do
+describe Referential, type: :model do
+  it "creates an Organisation with all features" do
     subscription = Subscription.new organisation_name: "organisation_test"
-
-    Feature.all.each do |feature|
-      expect(subscription.organisation.has_feature?(feature)).to be_truthy
-    end
+    expect(subscription.organisation.features).to match_array(Feature.all)
   end
 end
