4f41450f5a298b0eb74f74df3cbe83b45a5d7582
Zog <johan@about-blank.fr>
2018-07-18 11:36:26 +0200

    Fix ComplianceControlsController specs

--- spec/controllers/compliance_controls_controller_spec.rb
+++ spec/controllers/compliance_controls_controller_spec.rb
@@ -5,30 +5,9 @@ RSpec.describe ComplianceControlsController, type: :controller do
   let(:compliance_control)        { create(:generic_attribute_control_min_max) }
   let!(:compliance_control_set)   { compliance_control.compliance_control_set }
 
-  describe "GET show" do
-    it 'should be successful' do
-      get :show, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id
-      expect(response).to be_success
-    end
-  end
-
-  describe 'GET #edit' do
-    it 'should be successful' do
-      get :edit, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id
-      expect(response).to be_success
-    end
-  end
-
   describe 'GET #new' do
     it 'should be successful' do
       get :new, compliance_control_set_id: compliance_control_set.id, sti_class: 'GenericAttributeControl::MinMax'
-      expect(response).to be_success
-    end
-  end
-
-  describe 'GET #select_type' do
-    it 'should be successful' do
-      get :select_type, compliance_control_set_id: compliance_control_set.id
       expect(response).to be_success
     end
   end
@@ -41,19 +20,77 @@ RSpec.describe ComplianceControlsController, type: :controller do
     end
   end
 
-  describe 'POST #update' do
+  describe 'GET #select_type' do
     it 'should be successful' do
-      post :update, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id, compliance_control: compliance_control.as_json.merge(type: 'GenericAttributeControl::MinMax')
-      expect(response).to redirect_to compliance_control_set_path(compliance_control_set)
+      get :select_type, compliance_control_set_id: compliance_control_set.id
+      expect(response).to be_success
     end
   end
 
-  describe 'DELETE #destroy' do
-    it 'should be successful' do
-      expect {
-        delete :destroy, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id
-      }.to change(GenericAttributeControl::MinMax, :count).by(-1)
-      expect(response).to have_http_status(302)
+  context "on a control_set the user does not own" do
+    describe "GET show" do
+      it 'should be successful' do
+        get :show, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id
+        expect(response).to be_success
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'should be forbidden' do
+        get :edit, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id
+        expect(response).to have_http_status 403
+      end
+    end
+
+    describe 'POST #update' do
+      it 'should be forbidden' do
+        post :update, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id, compliance_control: compliance_control.as_json.merge(type: 'GenericAttributeControl::MinMax')
+        expect(response).to have_http_status 403
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'should be forbidden' do
+        expect {
+          delete :destroy, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id
+        }.to change(GenericAttributeControl::MinMax, :count).by(0)
+        expect(response).to have_http_status 403
+      end
+    end
+  end
+
+  context "on a control_set the user owns" do
+    before(:each){
+      compliance_control_set.update organisation: @user.organisation
+    }
+    describe "GET show" do
+      it 'should be successful' do
+        get :show, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id
+        expect(response).to be_success
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'should be successful' do
+        get :edit, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id
+        expect(response).to be_success
+      end
+    end
+
+    describe 'POST #update' do
+      it 'should be successful' do
+        post :update, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id, compliance_control: compliance_control.as_json.merge(type: 'GenericAttributeControl::MinMax')
+        expect(response).to redirect_to compliance_control_set_path(compliance_control_set)
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'should be successful' do
+        expect {
+          delete :destroy, compliance_control_set_id: compliance_control_set.id, id: compliance_control.id
+        }.to change(GenericAttributeControl::MinMax, :count).by(-1)
+        expect(response).to have_http_status(302)
+      end
     end
   end
 end
