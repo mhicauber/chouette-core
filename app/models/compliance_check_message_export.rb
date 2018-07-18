827cfd05848c65538a4d01df9d9a0434b818f7fd
Luc Donnet <luc.donnet@af83.com>
2018-07-18 23:12:48 +0200

    Fix i18n and csv header Refs #7600 @2

--- app/models/compliance_check_message_export.rb
+++ app/models/compliance_check_message_export.rb
@@ -22,13 +22,15 @@ class ComplianceCheckMessageExport
   end
 
   def column_names
-    ["criticity", "message_key", "message", "resource_objectid", "link"].map {|c| ComplianceCheckMessage.tmf(c)}
+    ["criticity", "message_key", "resource_objectid", "link", "message"].map {|c| ComplianceCheckMessage.tmf(c)}
   end
 
   def to_csv(options = {})
     csv_string = CSV.generate(options.slice(:col_sep, :quote_char, :force_quotes)) do |csv|
       csv << column_names
       compliance_check_messages.each do |compliance_check_message|
+        puts compliance_check_message.message_key
+        puts I18n.t("compliance_check_messages.#{compliance_check_message.message_key}", compliance_check_message.message_attributes.deep_symbolize_keys)
         csv << [compliance_check_message.compliance_check.criticity, *compliance_check_message.message_attributes.values_at('test_id', 'source_objectid'), options[:server_url] + compliance_check_message.message_attributes['source_object_path'], I18n.t("compliance_check_messages.#{compliance_check_message.message_key}", compliance_check_message.message_attributes.deep_symbolize_keys)]
       end
     end
