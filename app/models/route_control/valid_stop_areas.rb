9adfbcc51e67b6345a398336eb04d02edee7ac49
Zog <johan@about-blank.fr>
2018-07-18 17:02:17 +0200

    Refs #7600; New control is always enabled

--- app/models/route_control/valid_stop_areas.rb
+++ app/models/route_control/valid_stop_areas.rb
@@ -1,7 +1,5 @@
 module RouteControl
   class ValidStopAreas < InternalControl::Base
-    required_features :core_controls
-
     enumerize :criticity, in: %i(error), scope: true, default: :error
 
     def self.default_code; "3-Route-13" end
