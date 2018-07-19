313e664094d3237246a88b5234b99df586888cc6
Zog <johan@about-blank.fr>
2018-07-19 10:20:37 +0200

    Add a cron entry

--- app/mailers/audit_mailer.rb
+++ app/mailers/audit_mailer.rb
@@ -1,4 +1,8 @@
 class AuditMailer < ApplicationMailer
+  def self.enabled?
+    Rails.configuration.automated_audits_recipients.present?
+  end
+
   def audit opts={}
     @content = ReferentialAudit::Full.new.perform(opts.update({output: :html})).join("</td></tr><tr><td>")
     mail to: Rails.configuration.automated_audits_recipients, subject: t('mailers.audit_mailer.audit.subject', date: Time.now.l(format: :short))
--- config/initializers/cron.rb
+++ config/initializers/cron.rb
@@ -1,1 +1,2 @@
 Cron.every_5_minutes :check_import_operations, :check_ccset_operations
+Cron.every_day_at_3AM AuditMailer.audit().deliver if AuditMailer.enabled?
