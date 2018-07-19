73f96f794b2bee836381e2ae7ceeb764190d0e76
Zog <johan@about-blank.fr>
2018-07-19 10:20:37 +0200

    Add an optional limit on the number of audited referentials

--- app/services/referential_audit/full.rb
+++ app/services/referential_audit/full.rb
@@ -1,11 +1,20 @@
 class ReferentialAudit
   class Full
     def perform opts={}
+      limit = opts.delete(:limit)
       referentials = Referential.not_in_referential_suite.uniq
       referentials += Workbench.all.map { |w| w.output.current }.compact
+      referentials = referentials.uniq.sort_by(&:created_at).reverse
       out = []
-      referentials = referentials[0..5] if opts[:debug]
-      referentials.sort_by(&:created_at).uniq.reverse.each do |referential|
+      if limit
+        if limit.is_a? Integer
+          referentials = referentials[0..limit]
+        elsif limit.is_a?(Time) || limit.is_a?(Date)
+          referentials = referentials.select{|r| r.created_at > limit }
+        end
+      end
+
+      referentials.each do |referential|
         audit = ReferentialAudit::FullReferential.new referential
         out << audit.perform(plain_output: true)
       end
