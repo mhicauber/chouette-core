dbaeee8ef91dab9c050fc58f3d83ca55a368fada
Zog <johan@about-blank.fr>
2018-07-19 09:30:57 +0200

    Refs #7600; :fire: logs

--- app/models/compliance_check_message_export.rb
+++ app/models/compliance_check_message_export.rb
@@ -29,9 +29,12 @@ class ComplianceCheckMessageExport
     csv_string = CSV.generate(options.slice(:col_sep, :quote_char, :force_quotes)) do |csv|
       csv << column_names
       compliance_check_messages.each do |compliance_check_message|
-        puts compliance_check_message.message_key
-        puts I18n.t("compliance_check_messages.#{compliance_check_message.message_key}", compliance_check_message.message_attributes.deep_symbolize_keys)
-        csv << [compliance_check_message.compliance_check.criticity, *compliance_check_message.message_attributes.values_at('test_id', 'source_objectid'), options[:server_url] + compliance_check_message.message_attributes['source_object_path'], I18n.t("compliance_check_messages.#{compliance_check_message.message_key}", compliance_check_message.message_attributes.deep_symbolize_keys)]
+        csv << [
+          compliance_check_message.compliance_check.criticity,
+          *compliance_check_message.message_attributes.values_at('test_id', 'source_objectid'),
+          options[:server_url] + compliance_check_message.message_attributes['source_object_path'],
+          I18n.t("compliance_check_messages.#{compliance_check_message.message_key}", compliance_check_message.message_attributes.deep_symbolize_keys)
+          ]
       end
     end
     # We add a BOM to indicate we use UTF-8
