660e6024d59accf5f5a6899b57e7c6c808adeece
Kévin Bonnin <kevin.bonnin@af83.com>
2018-07-24 17:49:54 +0200

    Fix migration - l'oeuf ou la poule ?

--- db/migrate/20180717124110_associate_api_keys_to_workbench.rb
+++ db/migrate/20180717124110_associate_api_keys_to_workbench.rb
@@ -3,8 +3,8 @@ class AssociateApiKeysToWorkbench < ActiveRecord::Migration
     remove_column :api_keys, :referential_id
     add_column :api_keys, :workbench_id, :integer
 
-    Api::V1::ApiKey.where(workbench_id: nil).each do |key|
-      key.update_column(:workbench_id, key.organisation.workbenches.first&.id)
+    ApiKey.where(workbench_id: nil).each do |key|
+      key.update_column(:workbench_id, Organisation.find(key[:organisation_id]).workbenches.first&.id)
     end
 
     remove_column :api_keys, :organisation_id
