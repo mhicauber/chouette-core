a25f1523d2619a792a4b253557d8ac10d7732f2c
Kévin Bonnin <kevin.bonnin@af83.com>
2018-07-24 13:48:52 +0200

    Refs #7614 CRUD update

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -1,10 +1,43 @@
 class ApiKeysController < ChouetteController
+  defaults resource_class: ApiKey
+
   belongs_to :workbench
   include PolicyChecker
+
+  before_action :load_workbench
+
+  def index
+    index! do
+      @api_keys = ApiKeyDecorator.decorate(
+        collection,
+        context: { workbench: @workbench }
+      )
+    end
+  end
+
+  def create
+    create! do |success, _failure|
+      success.html do
+        redirect_to workbench_api_keys_path(@workbench)
+      end
+    end
+  end
+
+  def update
+    update! do |success, _failure|
+      success.html do
+        redirect_to workbench_api_keys_path(@workbench)
+      end
+    end
+  end
 
   private
 
   def api_key_params
     params.require(:api_key).permit(:name)
+  end
+
+  def load_workbench
+    @workbench = parent
   end
 end
