f13c7272ea12f476cf9aebb8ff03b585f03885df
Kévin Bonnin <kevin.bonnin@af83.com>
2018-07-25 10:07:27 +0200

    Refs #7614 remove unused spec

--- spec/routing/api/v1/access_links_routes_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Api::V1::AccessLinksController, type: :controller do
-  
-  it 'routes to index' do
-    expect( get: '/api/v1/access_links' ).to route_to(
-      controller: 'api/v1/access_links',
-      action: 'index'
-    )
-  end
-end
