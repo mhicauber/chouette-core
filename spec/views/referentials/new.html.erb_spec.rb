84d320129761eb2dc8c8bb4cbcd8472ac298a300
Zog <johan@about-blank.fr>
2018-08-03 12:35:05 +0200

    Fix specs

--- spec/views/referentials/new.html.erb_spec.rb
+++ spec/views/referentials/new.html.erb_spec.rb
@@ -4,6 +4,7 @@ describe "referentials/new", :type => :view do
 
   before(:each) do
     assign(:referential, Referential.new)
+    assign(:workbench, create(:workbench))
   end
 
   it "should have a textfield for name" do
