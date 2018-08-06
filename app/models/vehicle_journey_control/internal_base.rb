22adcfb91ca8a762f17efc1faa62988ecacd57e1
Zog <johan@about-blank.fr>
2018-08-06 11:57:06 +0200

    Refs #7816; Fix URL in Check error message

--- app/models/vehicle_journey_control/internal_base.rb
+++ app/models/vehicle_journey_control/internal_base.rb
@@ -1,7 +1,7 @@
 module VehicleJourneyControl
   class InternalBase < InternalControl::Base
     def self.object_path compliance_check, vehicle_journey
-      referential_line_route_vehicle_journeys_collection_path(compliance_check.referential, vehicle_journey.route.line, vehicle_journey.route)
+      referential_line_route_vehicle_journeys_path(compliance_check.referential, vehicle_journey.route.line, vehicle_journey.route)
     end
 
     def self.collection referential
