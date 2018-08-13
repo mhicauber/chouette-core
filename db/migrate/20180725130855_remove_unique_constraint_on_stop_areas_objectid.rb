971e05ed76817267276b227e37708221bec8a8de
Zog <johan@about-blank.fr>
2018-08-13 09:59:56 +0200

    Fix migrations

--- db/migrate/20180725130855_remove_unique_constraint_on_stop_areas_objectid.rb
+++ db/migrate/20180725130855_remove_unique_constraint_on_stop_areas_objectid.rb
@@ -1,6 +1,6 @@
 class RemoveUniqueConstraintOnStopAreasObjectid < ActiveRecord::Migration
   def change
-    remove_index :stop_areas, name: "stop_areas_objectid_key"
+    remove_index :stop_areas, name: "stop_areas_objectid_key" rescue nil
     add_index "stop_areas", ["objectid", "stop_area_referential_id"], unique: true, using: :btree, name: "stop_areas_objectid_key"
   end
 end
