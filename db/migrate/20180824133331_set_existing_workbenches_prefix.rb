632f88c2bfe6dde81700f5ba22defed13acf1b10
Zog <johan@about-blank.fr>
2018-08-24 15:51:38 +0200

    Fix sql

--- db/migrate/20180824133331_set_existing_workbenches_prefix.rb
+++ db/migrate/20180824133331_set_existing_workbenches_prefix.rb
@@ -1,5 +1,5 @@
 class SetExistingWorkbenchesPrefix < ActiveRecord::Migration
   def up
-    Workbench.connection.execute 'UPDATE "public"."workbenches" SET public.workbenches.prefix = organisations.code FROM "public"."workbenches" INNER JOIN "public"."organisations" ON "public"."organisations"."id" = "public"."workbenches"."organisation_id"  WHERE "public"."workbenches"."prefix" IS NULL'
+    Workbench.connection.execute 'UPDATE "public"."workbenches" SET prefix = organisations.code FROM "public"."organisations" WHERE "public"."organisations"."id" = "public"."workbenches"."organisation_id" AND "public"."workbenches"."prefix" IS NULL'
   end
 end
