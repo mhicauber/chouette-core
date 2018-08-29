bff752a1ac74deebe027dd493235aa48d91835dd
Zog <johan@about-blank.fr>
2018-08-29 17:10:05 +0200

    Refs #7502; Fix Calendar declaration

--- app/models/calendar/date_value.rb
+++ app/models/calendar/date_value.rb
@@ -1,32 +1,30 @@
-class Calendar < ApplicationModel
+require_dependency "../calendar"
 
-  class DateValue
-    include ActiveAttr::Model
+class Calendar::DateValue
+  include ActiveAttr::Model
 
-    attribute :id, type: Integer
-    attribute :value, type: Date
+  attribute :id, type: Integer
+  attribute :value, type: Date
 
-    validates_presence_of :value
+  validates_presence_of :value
 
-    def self.from_date(index, date)
-      new id: index, value: date
-    end
-
-    # Stuff required for coocon
-    def new_record?
-      !persisted?
-    end
-
-    def persisted?
-      id.present?
-    end
-
-    def mark_for_destruction
-      self._destroy = true
-    end
-
-    attribute :_destroy, type: Boolean
-    alias_method :marked_for_destruction?, :_destroy
+  def self.from_date(index, date)
+    new id: index, value: date
   end
 
+  # Stuff required for coocon
+  def new_record?
+    !persisted?
+  end
+
+  def persisted?
+    id.present?
+  end
+
+  def mark_for_destruction
+    self._destroy = true
+  end
+
+  attribute :_destroy, type: Boolean
+  alias_method :marked_for_destruction?, :_destroy
 end
