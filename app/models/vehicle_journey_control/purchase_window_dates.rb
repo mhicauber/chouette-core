c68c181ebb528e88b7c6773564b8512aef500f12
Zog <johan@about-blank.fr>
2018-08-31 12:07:56 +0200

    Refs #8025; Fix control when vj has no bouding dates

--- app/models/vehicle_journey_control/purchase_window_dates.rb
+++ app/models/vehicle_journey_control/purchase_window_dates.rb
@@ -1,7 +1,7 @@
 module VehicleJourneyControl
   class PurchaseWindowDates < InternalBase
     required_features :purchase_windows
-    
+
     def self.default_code; "3-VehicleJourney-7" end
 
     def self.collection referential
@@ -9,7 +9,7 @@ module VehicleJourneyControl
     end
 
     def self.compliance_test compliance_check, vehicle_journey
-      vehicle_journey.selling_bounding_dates.last <= vehicle_journey.bounding_dates.last
+      vehicle_journey.bounding_dates.empty? || vehicle_journey.selling_bounding_dates.last <= vehicle_journey.bounding_dates.last
     end
   end
 end
