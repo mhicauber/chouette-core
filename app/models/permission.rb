ee9625b04ba85469446a1d618cb9ed0704d8a008
Zog <johan@about-blank.fr>
2018-09-14 09:36:40 +0200

    #7899; Fix missing methods

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,36 +1,70 @@
 class Permission
-
-  def self.base
-    all_destructive_permissions + %w{sessions.create workbenches.update}
-  end
-
-  def self.extended
-    permissions = base
-
-    %w{purchase_windows exports}.each do |resources|
-      actions = %w{edit update create destroy}
-      actions.each do |action|
-        permissions << "#{resources}.#{action}"
-      end
+  class << self
+    def all_resources
+      %w[
+        access_points
+        connection_links
+        calendars
+        footnotes
+        imports
+        exports
+        merges
+        journey_patterns
+        referentials
+        routes
+        routing_constraint_zones
+        time_tables
+        vehicle_journeys
+        api_keys
+        compliance_controls
+        compliance_control_sets
+        compliance_control_blocks
+        compliance_check_sets
+        workbenches
+        workgroups
+      ]
     end
 
-    permissions << "calendars.share"
-  end
-
-  def self.referentials
-    permissions = []
-    %w{stop_areas stop_area_providers lines companies networks}.each do |resources|
-      actions = %w{edit update create}
-      actions << (%w{stop_areas lines}.include?(resources) ? "change_status" : "destroy")
-
-      actions.each do |action|
-        permissions << "#{resources}.#{action}"
-      end
+    def destructive_permissions_for(models)
+      models.product( %w{create destroy update} ).map{ |model_action| model_action.join('.') }
     end
-    permissions
-  end
 
-  def self.full
-    extended + referentials
+    def all_destructive_permissions
+      destructive_permissions_for( all_resources )
+    end
+
+    def base
+      all_destructive_permissions + %w{sessions.create workbenches.update}
+    end
+
+    def extended
+      permissions = base
+
+      %w{purchase_windows exports}.each do |resources|
+        actions = %w{edit update create destroy}
+        actions.each do |action|
+          permissions << "#{resources}.#{action}"
+        end
+      end
+
+      permissions << "calendars.share"
+    end
+
+    def referentials
+      permissions = []
+      %w{stop_areas stop_area_providers lines companies networks}.each do |resources|
+        actions = %w{edit update create}
+        actions << (%w{stop_areas lines}.include?(resources) ? "change_status" : "destroy")
+
+        actions.each do |action|
+          permissions << "#{resources}.#{action}"
+        end
+      end
+      permissions
+    end
+
+    def full
+      extended + referentials
+    end
   end
 end
