8aadf045a269e098c341488ca5a8cd28a3f0d440
Alban Peignier <alban.peignier@af83.com>
2018-09-19 13:14:30 +0200

    Change ImportsHelper#import_status to use a neutral color for canceled operations. Refs #8129

--- app/helpers/imports_helper.rb
+++ app/helpers/imports_helper.rb
@@ -8,16 +8,17 @@ module ImportsHelper
     i18n_prefix ||= "imports.status"
     status = status.to_s.downcase
     out = if %w[new running pending].include? status
-            content_tag :span, '', class: 'fa fa-clock-o'
-          else
-            cls = ''
-            cls = 'success' if status == 'successful'
-            cls = 'success' if status == 'ok'
-            cls = 'warning' if status == 'warning'
-            cls = 'danger' if %w[failed aborted canceled error].include? status
+      content_tag :span, '', class: "fa fa-clock-o"
+    else
+      cls = ''
+      cls = 'success' if status == 'successful'
+      cls = 'success' if status == 'ok'
+      cls = 'warning' if status == 'warning'
+      cls = 'info' if status == 'canceled'
+      cls = 'danger' if %w[failed aborted  error].include? status
 
-            content_tag :span, '', class: "fa fa-circle text-#{cls}"
-          end
+      content_tag :span, '', class: "fa fa-circle text-#{cls}"
+    end
     if verbose
       out += content_tag :span do
         txt = "#{i18n_prefix}.#{status}".t(fallback: "")
