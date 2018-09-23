b0218cbc482a0bd324c33d5e215fbd11c7e3cb1e
Alban Peignier <alban.peignier@af83.com>
2018-09-23 14:40:50 +0200

    Add test to avoid error in deploy in AddIndexOnVehicleJourneyJourneyPatternId. Refs #8255

--- db/migrate/20180905131655_add_options_to_imports.rb
+++ db/migrate/20180905131655_add_options_to_imports.rb
@@ -1,5 +1,6 @@
 class AddOptionsToImports < ActiveRecord::Migration
   def change
+    # Avoid error in the case of issue #8255
     return if ActiveRecord::Base.connection.column_exists?(:imports, :options)
 
     add_column :imports, :options, :hstore
--- db/migrate/20180914100755_add_index_on_vehicle_journey_journey_pattern_id.rb
+++ db/migrate/20180914100755_add_index_on_vehicle_journey_journey_pattern_id.rb
@@ -1,5 +1,7 @@
 class AddIndexOnVehicleJourneyJourneyPatternId < ActiveRecord::Migration
   def change
+    # Avoid error in the case of issue #8255
+    return if ActiveRecord::Base.connection.index_exists?(:vehicle_journeys, :journey_pattern_id)
     add_index :vehicle_journeys, :journey_pattern_id
   end
 end
