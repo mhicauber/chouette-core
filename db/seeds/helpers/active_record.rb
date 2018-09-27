a73c69aa21a16bdcc2696a499bc3607f585ba106
Zog <johan@about-blank.fr>
2018-09-27 15:51:59 +0200

    Refs #8448; Add `deleted_user`in seeds

--- db/seeds/helpers/active_record.rb
+++ db/seeds/helpers/active_record.rb
@@ -4,7 +4,14 @@ class ActiveRecord::Base
     print "Seed #{name} #{key_attribute.inspect} "
     yield model
 
-    puts "[#{(model.changed? ? 'updated' : 'no change')}]"
+    status = if model.new_record?
+      'created'
+    elsif model.changed?
+      'updated'
+    else
+      'no change'
+    end
+    puts "[#{status}]"
     unless model.save
       puts "Can't save #{model.class.name} : #{model.errors.inspect}"
       raise ActiveRecord::RecordNotSaved.new("Invalid #{model.class.name} : #{model.errors.inspect}")
