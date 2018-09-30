fc893f30d7e869671ff32e35fbc947a614959897
cedricnjanga <ced.njg@gmail.com>
2018-09-30 10:27:26 -0700

    Refs #8273 Add Empty timetable compliance control

---
+++ app/models/vehicle_journey_control/empty_time_table.rb
@@ -0,0 +1,19 @@
+module VehicleJourneyControl
+  class EmptyTimeTable < InternalBase
+    enumerize :criticity, in: %i(error), scope: true, default: :error
+
+    def self.default_code; "3-VehicleJourney-10" end
+
+    def self.compliance_test compliance_check, vehicle_journey
+      vehicle_journey.time_tables.empty.empty?
+    end
+
+    def self.custom_message_attributes compliance_check, vj
+      {
+        source_objectid: vj.objectid,
+        vj_name: vj.published_journey_name,
+        tt_names: vj.time_tables.empty.pluck(:comment).to_sentence
+        }
+    end
+  end
+end
