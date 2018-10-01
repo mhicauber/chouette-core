f1243183e8ff4572a9c6fc3cb58f1340997275b0
Zog <johan@about-blank.fr>
2018-10-01 09:52:35 +0200

    Refs #8157; Small refactor

--- app/models/concerns/sync_support.rb
+++ app/models/concerns/sync_support.rb
@@ -1,11 +1,19 @@
 module SyncSupport
   extend ActiveSupport::Concern
-  KEEP_SYNCS = 40
+
+  included do
+    @keep_syncs = 40
+
+    class << self
+      attr_accessor :keep_syncs
+    end
+  end
 
   def clean_previous_syncs(sync_type)
-    return unless send(sync_type).count > KEEP_SYNCS
-    while send(sync_type).count > KEEP_SYNCS do
-      send(sync_type).order("created_at asc").first.destroy
+    collection = send(sync_type)
+    return unless collection.count > self.class.keep_syncs
+    while collection.count > self.class.keep_syncs do
+      collection.order("created_at asc").first.destroy
     end
   end
 end
