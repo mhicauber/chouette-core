af0acf11f87deecbd58cc44fc8f70d1ade025f76
cedricnjanga <ced.njg@gmail.com>
2018-10-03 08:29:31 -0700

    Refs #7606 orders tt after pagination

--- app/controllers/autocomplete_time_tables_controller.rb
+++ app/controllers/autocomplete_time_tables_controller.rb
@@ -19,7 +19,7 @@ class AutocompleteTimeTablesController < ChouetteController
     if params[:route_id]
       scope = scope.joins(vehicle_journeys: :route).where( "routes.id IN (#{params[:route_id]})")
     end
-    scope.distinct.select('*, lower(comment) AS lower_comment').order("lower_comment ASC").all
+    scope.distinct
   end
 
   def split_params! search
@@ -28,6 +28,6 @@ class AutocompleteTimeTablesController < ChouetteController
 
   def collection
     split_params! :unaccented_comment_or_objectid_cont_any
-    @time_tables = select_time_tables.search(params[:q]).result.paginate(page: params[:page])
+    @time_tables = select_time_tables.search(params[:q]).result.paginate(page: params[:page]).select('*, lower(comment) AS lower_comment').order("lower_comment ASC")
   end
 end
