9f1557c34186c1532100fc3fcbb2ae587cdb96fc
Alban Peignier <alban.peignier@af83.com>
2018-11-08 21:27:27 +0100

    Change belong_to validate presences to check instances instead of identifiers. Refs #7694

--- spec/models/workgroup_spec.rb
+++ spec/models/workgroup_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Workgroup, type: :model do
   context "associations" do
-    let( :workgroup ){ build_stubbed :workgroup, line_referential_id: 53, stop_area_referential_id: 42 }
+    let(:stop_area_referential) { build_stubbed :stop_area_referential }
+    let(:line_referential) { build_stubbed :line_referential }
+
+    let(:workgroup){ build_stubbed :workgroup, line_referential: line_referential, stop_area_referential: stop_area_referential }
 
     it{ should have_many(:workbenches) }
     it{ should validate_uniqueness_of(:name) }
@@ -10,15 +13,15 @@ RSpec.describe Workgroup, type: :model do
     it{ should validate_uniqueness_of(:line_referential_id) }
 
     it 'is not valid without a stop_area_referential' do
-      workgroup.stop_area_referential_id = nil
+      workgroup.stop_area_referential = nil
       expect( workgroup ).not_to be_valid
     end
     it 'is not valid without a line_referential' do
-      workgroup.line_referential_id = nil
+      workgroup.line_referential = nil
       expect( workgroup ).not_to be_valid
     end
-    it 'is valid with both assoications' do
-      expect( workgroup ).to be_valid
+    it 'is valid with both associations' do
+      expect(workgroup).to be_valid
     end
   end
 
