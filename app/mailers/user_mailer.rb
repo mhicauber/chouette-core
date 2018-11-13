5cdb68d0ed24ad2f0fd6da928b6866c7a60bb496
kevin <kevin.bonnin@af83.com>
2018-11-13 15:57:39 +0100

    Refs #9126 remove user observer and mailer

--- app/mailers/user_mailer.rb
+++
@@ -1,7 +0,0 @@
-class UserMailer < ApplicationMailer
-
-  def created user_id, email
-    @user      = User.find(user_id)
-    mail to: email, subject: t('mailers.user_mailer.created.subject')
-  end
-end
