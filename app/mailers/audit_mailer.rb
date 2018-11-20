2011550e2ff13ff7831e34c0ba6e5bda79196822
Zog <johan@about-blank.fr>
2018-11-20 10:44:20 +0100

    Refs #9278; Fix Audit cron syntax

--- app/mailers/audit_mailer.rb
+++ app/mailers/audit_mailer.rb
@@ -3,6 +3,11 @@ class AuditMailer < ApplicationMailer
     !!Rails.configuration.enable_automated_audits
   end
 
+  def self.audit_if_enabled opts={}
+    return unless enabled?
+    audit(opts).deliver
+  end
+
   def audit opts={}
     @content = ReferentialAudit::Full.new.perform(opts.update({output: :html})).join("</td></tr><tr><td>")
     mail to: Rails.configuration.automated_audits_recipients, subject: t('mailers.audit_mailer.audit.subject', date: Time.now.l(format: :short))
