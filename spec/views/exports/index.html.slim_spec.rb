957a8d19c38c428e4d7fd5e1ddc99adff6350264
Zog <johan@about-blank.fr>
2018-11-23 16:22:16 +0100

    Refs #9156; Fix specs

--- spec/views/exports/index.html.slim_spec.rb
+++ spec/views/exports/index.html.slim_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe "/exports/index", :type => :view do
+RSpec.describe '/exports/index', :type => :view do
 
   let(:ref) { create :workbench_referential }
   let(:workbench) { assign :workbench, ref.workbench }
@@ -14,37 +14,41 @@ RSpec.describe "/exports/index", :type => :view do
 
   before(:each) do
     allow(view).to receive(:collection_name).and_return('exports')
-    allow(view).to receive(:collection_class_name).and_return(Export::Base)
+    allow(view).to receive(:index_model).and_return(Export::Base)
     allow(view).to receive(:collection_path).and_return(workbench_exports_path(workbench))
     allow(view).to receive(:collection).and_return(exports)
     allow(view).to receive(:decorated_collection).and_return(exports)
     allow(view).to receive(:params).and_return({action: :index})
     controller.request.path_parameters[:workbench_id] = workbench.id
     controller.request.path_parameters[:action] = "index"
-    render
+
   end
 
   context "referential name" do
     context "export with referential" do
-      xit "should have referential name on page" do
+      before(:each) do
+        render
+      end
+      it "should have referential name on page" do
         exports.each_with_index do |export, index|
           ref_link = link_to(export.referential.name, href: referential_path(export.referential))
           expect(export.referential.present?).to be_truthy
-          expect(view).to have_link(export.referential.name, href: referential_path(export.referential))
-          expect(view).to have_link(export.name, href: workbench_export_path(export))
+          expect(rendered).to have_link(export.referential.name, href: referential_path(export.referential))
+          expect(rendered).to have_link(export.name, href: workbench_export_path(workbench, export))
         end
       end
     end
     context "export without referential" do
       before(:each) do
         ref.destroy
+        render
       end
-      xit "should not have show link(s) to redirect to show page" do
+      it "should not have show link(s) to redirect to show page" do
         exports.each_with_index do |export, index|
           expect(export.reload.referential.present?).to be_falsy
-          expect(view).not_to have_link(export.referential.name, href: referential_path(export.referential))
-          expect(view).to have_link(export.name, href: workbench_export_path(export))
-          expect(view).to have_xpath("//tr[contains(concat(' ',normalize-space(@cla#ss),' '),' base ')][#{index + 1}]/td[4][contains(., '-')]")
+          expect(rendered).not_to have_link(ref.name, href: referential_path(ref.id))
+          expect(rendered).to have_link(export.name, href: workbench_export_path(workbench, export))
+          expect(rendered).to have_xpath("//tr[contains(@class,'base base-#{export.id}')]")
         end
       end
     end
