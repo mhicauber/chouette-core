42242353457fafe51cfe8c8ff695f9f7ea7deda2
cedricnjanga <ced.njg@gmail.com>
2018-12-10 08:44:45 -0800

    Refs #9549 Add merge actions links

--- app/controllers/workbench_outputs_controller.rb
+++ app/controllers/workbench_outputs_controller.rb
@@ -4,6 +4,18 @@ class WorkbenchOutputsController < ChouetteController
 
   def show
     @workbench = current_organisation.workbenches.find params[:workbench_id]
-    @workbench_merges = @workbench.merges.order("created_at desc").paginate(page: params[:page], per_page: 10)
+    workbench_merges = @workbench.merges.order("created_at desc").paginate(page: params[:page], per_page: 10)
+    @workbench_merges = decorate_merges(workbench_merges)
+  end
+
+  private
+
+  def decorate_merges(merges)
+    MergeDecorator.decorate(
+      merges,
+      context: { 
+        workbench: @workbench
+      }
+    )
   end
 end
