19693653976c3b6e1822bf0affd74dc3e8a9e1f9
cedricnjanga <ced.njg@gmail.com>
2018-12-10 23:35:13 -0800

    Refs #9570 Add data confirm modal to merge#rollback

--- app/decorators/merge_decorator.rb
+++ app/decorators/merge_decorator.rb
@@ -12,6 +12,7 @@ class MergeDecorator < AF83::Decorator
       l.href do
         h.rollback_workbench_merge_path(context[:workbench],object)
       end
+      l.data {{ confirm: h.t('merges.actions.rollback_confirm') }}
     end
   end
 end
