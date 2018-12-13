e2a97b6c67a4e91f60bc8373104a3feaf2f5ec1c
Zog <johan@about-blank.fr>
2018-12-13 12:39:08 +0100

    Refs #9610; Remove empty metadata when cloning e Referential

--- spec/models/merge_spec.rb
+++ spec/models/merge_spec.rb
@@ -5,12 +5,10 @@ RSpec.describe Merge do
   let(:line_referential){ create :line_referential }
   let(:company){ create :company, line_referential: line_referential }
   let(:workbench){ create :workbench, line_referential: line_referential, stop_area_referential: stop_area_referential }
-  let(:referential_metadata){ create(:referential_metadata, lines: line_referential.lines.limit(3)) }
   let(:referential){
-    create :referential,
-      workbench: workbench,
-      organisation: workbench.organisation,
-      metadatas: [referential_metadata]
+    ref = create :referential, workbench: workbench, organisation: workbench.organisation
+    create(:referential_metadata, lines: line_referential.lines.limit(3), referential: ref)
+    ref.reload
   }
 
   before(:each) do
@@ -207,6 +205,27 @@ RSpec.describe Merge do
   end
 
   context "#prepare_new" do
+    context "when some lines are no longer available", truncation: true do
+      let(:merge){Merge.create(workbench: workbench, referentials: [referential, referential]) }
+
+      before do
+        ref = create(:workbench_referential, workbench: workbench)
+        create(:referential_metadata, lines: [line_referential.lines.first], referential: ref)
+        create(:referential_metadata, lines: [line_referential.lines.last], referential: ref)
+        allow(workbench).to receive(:lines){ Chouette::Line.where(id: line_referential.lines.last.id) }
+        workbench.output.update current: ref.reload
+      end
+
+      after(:each) do
+        Apartment::Tenant.drop(workbench.output.current.slug)
+        Apartment::Tenant.drop(referential.slug)
+      end
+
+      it "should work" do
+        expect{ merge.prepare_new }.to_not raise_error
+      end
+    end
+
     context "with no current output" do
       let(:merge){Merge.create(workbench: workbench, referentials: [referential, referential]) }
 
