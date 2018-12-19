18ed818e8faca13b054e81d1fc14b4b9cb19b7da
Zog <johan@about-blank.fr>
2018-12-19 09:14:39 +0100

    Refs #9530; Fix objectid filter

--- app/models/chouette/objectid_formatter/base.rb
+++ app/models/chouette/objectid_formatter/base.rb
@@ -4,7 +4,11 @@ module Chouette
       include ::ActiveRecord::Sanitization
 
       def with_short_id scope, q
-        scope.where("#{short_id_sql_expr} LIKE '%#{self.class.send(:sanitize_sql_like, q&.downcase)}%'")
+        scope.where("#{short_id_sql_expr(scope)} LIKE '%#{self.class.send(:sanitize_sql_like, q&.downcase)}%'")
+      end
+
+      def table_name(model_class)
+        model_class.table_name.split(".").last
       end
     end
   end
