6ca86e0ca9ed36b8b0e9dd3839c170fcc9967e2d
Zog <johan@about-blank.fr>
2018-12-21 16:05:02 +0100

    Refs #9704; :lipstick:

--- app/controllers/export_uploads_controller.rb
+++ app/controllers/export_uploads_controller.rb
@@ -1,10 +1,6 @@
 class ExportUploadsController < ApplicationController
-  # include PolicyChecker
-  # include RansackDateFilter
-  # include IevInterfaces
   skip_before_action :authenticate_user!, only: [:upload]
   skip_before_action :verify_authenticity_token, only: [:upload]
-  # defaults resource_class: Export::Base, collection_name: 'exports', instance_name: 'export'
 
   def upload
     resource = Export::Base.find params[:id]
