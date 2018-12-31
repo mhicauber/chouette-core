a3631b4b5b1627597568f99208e4bdea4a4b2e27
cedricnjanga <ced.njg@gmail.com>
2018-12-30 22:45:20 -0800

    Refs #9733 Add ReferentialSuite#referentials_created_before_current

--- spec/models/referential_suite/referential_suite_spec.rb
+++ spec/models/referential_suite/referential_suite_spec.rb
@@ -2,4 +2,26 @@ RSpec.describe ReferentialSuite, type: :model do
   it { should belong_to(:new).class_name('Referential') }
   it { should belong_to(:current).class_name('Referential') }
   it { should have_many(:referentials) }
+  
+  describe "#referentials_created_before_current" do
+    let!(:referential_suite) { create :referential_suite }
+    let!(:ref1) { create :referential, created_at: Time.now - 10.days, referential_suite_id: referential_suite.id }
+    let!(:ref2) { create :referential, created_at: Time.now + 10.days, referential_suite_id: referential_suite.id }
+
+    context "without current output" do
+      it "should only return all the referentials" do
+        expect(referential_suite.reload.referentials_created_before_current.to_a).to match_array([ref1, ref2])
+      end
+    end
+
+    context "with current output" do
+      before do
+        referential_suite.current = create(:referential, created_at: Time.now, referential_suite_id: referential_suite.id)
+        referential_suite.save
+      end
+      it "should only return the referentials created before the current output" do
+        expect(referential_suite.reload.referentials_created_before_current.to_a).to match_array([ref1])
+      end
+    end
+  end
 end
