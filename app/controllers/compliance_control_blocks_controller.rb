3c3143f15184a167792d41605640e74d13a1b687
Zog <johan@about-blank.fr>
2019-01-02 16:46:38 +0100

    Refs #9563; Add an optional feature
    That grants access to the new block kind

--- app/controllers/compliance_control_blocks_controller.rb
+++ app/controllers/compliance_control_blocks_controller.rb
@@ -2,7 +2,8 @@ class ComplianceControlBlocksController < ChouetteController
   include PolicyChecker
   defaults resource_class: ComplianceControlBlock
   belongs_to :compliance_control_set
-  actions :all, :except => [:show, :index]
+  actions :all, except: %i[show index]
+  before_action :load_block_kind, only: %i[new edit]
 
   private
 
@@ -14,5 +15,10 @@ class ComplianceControlBlocksController < ChouetteController
 
   alias_method :compliance_control_set, :parent
   helper_method :compliance_control_set
+
+  def load_block_kind
+    @block_kinds = %w[transport_mode]
+    @block_kinds << :stop_areas_in_countries if has_feature?(:core_control_blocks)
+  end
 
 end
