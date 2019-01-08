ff2cd81c0af0aeef454bf5da85bf26b8daf65dec
Zog <johan@about-blank.fr>
2019-01-08 13:42:16 +0100

    Update Feature.all

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,5 +1,25 @@
 class Feature
   def self.all
-    %w{application_days_on_calendars change_locale consolidated_offers core_controls costs_in_journey_patterns create_opposite_routes detailed_calendars detailed_purchase_windows journey_length_in_vehicle_journeys long_distance_routes purchase_windows referential_vehicle_journeys route_stop_areas_all_types stop_area_localized_names stop_area_waiting_time vehicle_journeys_return_route routing_constraint_zone_exclusion_in_vehicle_journey}
-  end
+    %w{
+      application_days_on_calendars
+      change_locale
+      consolidated_offers
+      core_controls
+      core_control_blocks
+      costs_in_journey_patterns
+      create_opposite_routes
+      detailed_calendars
+      detailed_purchase_windows
+      journey_length_in_vehicle_journeys
+      long_distance_routes
+      manage_publications
+      purchase_windows
+      referential_vehicle_journeys
+      route_stop_areas_all_types
+      routing_constraint_zone_exclusion_in_vehicle_journey
+      stop_area_localized_names
+      stop_area_waiting_time
+      vehicle_journeys_return_route
+    }
+ end
 end
