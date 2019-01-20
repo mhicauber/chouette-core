656613fcf9ba120c2bc2ccaa428a07bda0351c11
cedricnjanga <ced.njg@gmail.com>
2019-01-19 20:30:00 -0800

    Refs #9958 Allow all  panel items to have the same behaviour

--- app/helpers/menus_helper.rb
+++ app/helpers/menus_helper.rb
@@ -2,14 +2,15 @@ module MenusHelper
   def main_nav_menu_item label, &block
     @current_menu_item_count ||= 0
     @current_menu_item_count += 1
+    id = "menu-item-#{@current_menu_item_count}"
     content_tag :div, class: "menu-item panel" do
       out = ""
       out += content_tag(:div, class: "panel-heading") do
         content_tag :h4, class: "panel-title" do
-          link_to label, "#menu-item-#{@current_menu_item_count}", data: {toggle: 'collapse', parent: '#menu-items'}, 'aria-expanded' => 'false'
+          link_to label, "#menu-item-#{@current_menu_item_count}", data: { toggle: 'collapse' }, 'aria-expanded' => 'false', 'aria-controls' => id
         end
       end
-      out += content_tag(:div, class: "panel-collapse collapse", id: "menu-item-#{@current_menu_item_count}") do
+      out += content_tag(:div, class: "panel-collapse collapse", id: id) do
         content_tag :div, class: "list-group" do
           capture(&block)
         end
