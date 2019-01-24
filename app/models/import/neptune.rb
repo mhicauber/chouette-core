4a51da3280ffbf5f68af6f40905fbf04569cdcc8
Zog <johan@about-blank.fr>
2019-01-24 14:42:29 +0100

    Refs #10176; Import lines in Neptune import

--- app/models/import/neptune.rb
+++ app/models/import/neptune.rb
@@ -19,18 +19,83 @@ class Import::Neptune < Import::Base
   end
 
   def prepare_referential
-    # import_resources :lines
+    import_resources :lines
 
     create_referential
     referential.switch
   end
 
   def referential_metadata
-    # TODO #10176
-    line_ids = line_referential.lines.pluck :id
-
     # TODO #10177
     periode = (Time.now..1.month.from_now)
-    ReferentialMetadata.new line_ids: line_ids, periodes: [periode]
+    ReferentialMetadata.new line_ids: @imported_line_ids, periodes: [periode]
+  end
+
+  protected
+  def each_source
+    Zip::File.open(local_file) do |zip_file|
+      zip_file.glob('*.xml').each do |f|
+        yield Nokogiri::XML(f.get_input_stream)
+      end
+    end
+  end
+
+  def import_lines
+    each_source do |source|
+      source.css('ChouettePTNetwork ChouetteLineDescription Line')\
+            .map(&method(:build_object_from_nokogiri_element))\
+            .each do |source_line|
+
+        line = line_referential.lines.find_or_initialize_by registration_number: source_line[:object_id]
+        line.name = source_line[:name]
+        line.number = source_line[:number]
+        line.published_name = source_line[:published_name]
+        line.comment = source_line[:comment]
+        line.transport_mode, line.transport_submode = transport_mode_name_mapping(source_line[:transport_mode_name])
+
+        save_model line
+        @imported_line_ids ||= []
+        @imported_line_ids << line.id
+      end
+    end
+  end
+
+  def transport_mode_name_mapping(source_transport_mode)
+    {
+      'Air' => nil,
+      'Train' => ['rail', 'regionalRail'],
+      'LongDistanceTrain' => ['rail', 'interregionalRail'],
+      'LocalTrain' => ['rail', 'suburbanRailway'],
+      'RapidTransit' => ['rail', 'railShuttle'],
+      'Metro' => ['metro', nil],
+      'Tramway' => ['tram', nil],
+      'Coach' => ['bus', nil],
+      'Bus' => ['bus', nil],
+      'Ferry' => nil,
+      'Waterborne' => nil,
+      'PrivateVehicle' => nil,
+      'Walk' => nil,
+      'Trolleybus' => ['tram', nil],
+      'Bicycle' => nil,
+      'Shuttle' => ['bus', 'airportLinkBus'],
+      'Taxi' => nil,
+      'VAL' => ['rail', 'railShuttle'],
+      'Other' => nil
+    }[source_transport_mode]
+  end
+
+  def build_object_from_nokogiri_element(element)
+    out = {}
+    element.children.each do |child|
+      key = child.name.underscore.to_sym
+      next if key == :text
+      if child.children.count == 1 && child.children.last.node_type == Nokogiri::XML::Node::TEXT_NODE
+        content = child.children.last.content
+        out[key] = content
+      else
+        out[key] = build_object_from_nokogiri_element(child)
+      end
+    end
+    out
   end
 end
--- spec/models/import/neptune_spec.rb
+++ spec/models/import/neptune_spec.rb
@@ -40,13 +40,38 @@ RSpec.describe Import::Neptune do
     let(:import) { build_import }
 
     before(:each) do
-      create :line, line_referential: workbench.line_referential 
+      create :line, line_referential: workbench.line_referential
     end
 
     it "is named after the import name" do
       import.name = "Import Name"
       import.create_referential
       expect(import.referential.name).to eq(import.name)
+    end
+
+    it 'uses the imported lines in the metadata' do
+      import.send(:import_lines)
+      new_line = workbench.line_referential.lines.last
+      import.create_referential
+      expect(import.referential.metadatas.count).to eq 1
+      expect(import.referential.metadatas.last.line_ids).to eq [new_line.id]
+    end
+  end
+
+  describe "#import_lines" do
+    let(:import) { build_import }
+
+    it 'should create new lines' do
+      expect{ import.send(:import_lines) }.to change{ workbench.line_referential.lines.count }.by 1
+    end
+
+    it 'should update existing lines' do
+      import.send(:import_lines)
+      line = workbench.line_referential.lines.last
+      attrs = line.attributes.except('updated_at')
+      line.update transport_mode: :tram, published_name: "foo"
+      expect{ import.send(:import_lines) }.to_not change{ workbench.line_referential.lines.count }
+      expect(line.reload.attributes.except('updated_at')).to eq attrs
     end
   end
 end
