c6f485c50c6610689d108bfea80b272290ab1422
Zog <johan@about-blank.fr>
2019-01-30 19:07:48 +0100

    Fix specs

--- spec/controllers/publication_apis_controller_spec.rb
+++ spec/controllers/publication_apis_controller_spec.rb
@@ -8,71 +8,72 @@ RSpec.describe PublicationApisController, type: :controller do
   let(:workgroup){ workbench.workgroup }
   let(:publication_api_params){ { name: "Demo", slug: "demo" } }
   let(:publication_api){ create(:publication_api, workgroup: workgroup) }
-
-  describe "GET index" do
-    let(:request){ get :index, workgroup_id: workgroup.id }
-    it "should be ok" do
-      request
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET new" do
-    let(:request){ get :new, workgroup_id: workgroup.id }
-    it "should not be ok" do
-      request
-      expect(response).not_to be_success
-    end
-
-    with_permission "publication_apis.create" do
+  with_feature "manage_publications" do
+    describe "GET index" do
+      let(:request){ get :index, workgroup_id: workgroup.id }
       it "should be ok" do
         request
         expect(response).to be_success
       end
     end
-  end
 
-  describe "POST create" do
-    let(:request){ post :create, workgroup_id: workgroup.id, publication_api: publication_api_params }
-    it "should not be ok" do
-      request
-      expect(response).not_to be_success
-    end
-
-    with_permission "publication_apis.create" do
-      it "should be ok" do
+    describe "GET new" do
+      let(:request){ get :new, workgroup_id: workgroup.id }
+      it "should not be ok" do
         request
-        expect(response).to be_redirect
+        expect(response).not_to be_success
+      end
+
+      with_permission "publication_apis.create" do
+        it "should be ok" do
+          request
+          expect(response).to be_success
+        end
       end
     end
-  end
 
-  describe "GET edit" do
-    let(:request){ get :edit, workgroup_id: workgroup.id, id: publication_api.id }
-    it "should not be ok" do
-      request
-      expect(response).not_to be_success
-    end
-
-    with_permission "publication_apis.update" do
-      it "should be ok" do
+    describe "POST create" do
+      let(:request){ post :create, workgroup_id: workgroup.id, publication_api: publication_api_params }
+      it "should not be ok" do
         request
-        expect(response).to be_success
+        expect(response).not_to be_success
+      end
+
+      with_permission "publication_apis.create" do
+        it "should be ok" do
+          request
+          expect(response).to be_redirect
+        end
       end
     end
-  end
 
-  describe "POST update" do
-    let(:request){ post :update, workgroup_id: workgroup.id, id: publication_api.id, publication_api: publication_api_params }
-    it "should not be ok" do
-      request
-      expect(response).not_to be_success
+    describe "GET edit" do
+      let(:request){ get :edit, workgroup_id: workgroup.id, id: publication_api.id }
+      it "should not be ok" do
+        request
+        expect(response).not_to be_success
+      end
+
+      with_permission "publication_apis.update" do
+        it "should be ok" do
+          request
+          expect(response).to be_success
+        end
+      end
     end
 
-    with_permission "publication_apis.update" do
-      it "should be ok" do
+    describe "POST update" do
+      let(:request){ post :update, workgroup_id: workgroup.id, id: publication_api.id, publication_api: publication_api_params }
+      it "should not be ok" do
         request
-        expect(response).to be_redirect
+        expect(response).not_to be_success
+      end
+
+      with_permission "publication_apis.update" do
+        it "should be ok" do
+          request
+          expect(response).to be_redirect
+        end
       end
     end
   end
