97ecf760783e52e8c5c6c790eba168a87f78ce2d
Alban Peignier <alban.peignier@af83.com>
2019-02-19 21:39:19 +0100

    Remove migration which add by default NeTEx full export. Refs #8639

--- db/migrate/20190218074447_add_netex_full_to_workgroups.rb
+++
@@ -1,8 +0,0 @@
-class AddNetexFullToWorkgroups < ActiveRecord::Migration
-  def change
-    Workgroup.where.not("export_types::text[] @> ARRAY['Export::NetexFull']").find_each do |w|
-      w.export_types << "Export::NetexFull"
-      w.save!
-    end
-  end
-end
